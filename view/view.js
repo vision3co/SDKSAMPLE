06316f3d2588de8cec1c510716a2e807ba93173e
Nasimi <Nasimiasl@live.com>
2026-04-13 18:04:14 +0400

    add items for planner

---
+++ view/view.js
@@ -0,0 +1,14 @@
+
+/// asset room set 
+{
+
+    name:'roomset1',
+    data : { /* AI room design */} ,
+    views : [
+        { name:'' ,data:{ 
+            background:'image',
+            product1_sec_layer : 'image' }
+            /**  */ } ,  alpha:0,beta:0,radius:0, target:[0,0,0] },
+    ]
+
+}
